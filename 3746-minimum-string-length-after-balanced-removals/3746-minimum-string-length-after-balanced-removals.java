fcdde6d64beae867310604568f94ed5a5e7057ed
shanmugaravi577-commits <shanmugaravi577@gmail.com>
2026-06-29 21:20:51 +0530

    Time: 3 ms (98.84%), Space: 47.3 MB (37.79%) - LeetHub

--- 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
@@ -1,12 +1,10 @@
 class Solution {
     public int minLengthAfterRemovals(String s) {
-        int n=s.length();
-        if(n==1)return 1;
         int ac=0,bc=0;
-        for(int i=0;i<n;i++)
+        for(char k:s.toCharArray())
         {
-            char c=s.charAt(i);
-            if(c == 'a')
+          
+            if(k == 'a')
              ac++;
             else
              bc++;
