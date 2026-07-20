705d0b47ff43ce7486f6324919012a3ea3c2d46f
shanmugaravi577-commits <shanmugaravi577@gmail.com>
2026-07-20 22:55:48 +0530

    Time: 5 ms (3.56%), Space: 61 MB (93.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,7 +1,9 @@
 class Solution {
    void reverse(int a[],int s,int e)
-   {int j=e-1;
-       for(int i =s ;i<(s+e)/2;i++)
+   {
+       int j=e-1;
+        System.out.println((s+e)/2);
+       for(int i =s;i<(s+e)/2;i++)
        {
           int c = a[i];
           a[i]= a[j];
@@ -12,6 +14,7 @@ class Solution {
     public void rotate(int[] nums, int k) {
         int n=nums.length;
         k=k%n;
+        System.out.println(n+" "+k);
         reverse(nums,0,n);
         reverse(nums,0,k);
         reverse(nums,k,n);
