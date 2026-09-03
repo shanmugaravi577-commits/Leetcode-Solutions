7c02f01c6d6add08b2fff9b4921bc767114954bf
shanmugaravi577-commits <shanmugaravi577@gmail.com>
2026-09-03 20:00:41 +0530

    Time: 15 ms (54.95%), Space: 47.4 MB (91.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,28 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int n:nums)
-        {
-             map.put(n,map.getOrDefault(n,0)+1);
-        }
-        System.out.println(map);
-     List<int[]> l = new ArrayList<>();
-     for(Map.Entry<Integer,Integer> entry:map.entrySet())
-     {
-        int key = entry.getKey();
-        int v= entry.getValue();
-        l.add(new int[]{key,v});
-     }
-     Collections.sort(l,(x,y)->{
-        return Integer.compare(y[1],x[1]);
-     });
-   int [] ans = new int[k];
+Map<Integer,Integer> map =new HashMap<>();
+for(int p:nums)
+map.put(p,map.getOrDefault(p,0)+1);
 
-   for(int i=0;i<k;i++)
-    {
-        int[] temp = l.get(i);
-        ans[i]=temp[0];
-    }
+List<int[]> l=new ArrayList<>();
+for(Map.Entry<Integer,Integer> entry:map.entrySet())
+{
+    int arr[] =  new int[2];
+    arr[0]=entry.getKey();
+    arr[1]=entry.getValue();
+    l.add(arr);
+}
+Collections.sort(l,(a,b)->Integer.compare(b[1],a[1]));
+int ans[]=new int[k];
+for(int i=0;i<k;i++)
+{
+    int a[]=l.get(i);
+    ans[i]=a[0];
+}
 return ans;
     }
 }
